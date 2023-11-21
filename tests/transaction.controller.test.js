2f5c9f648e3da1ac61b0e07d222bae076420ba90
adefolajuwon oyebola <oadefolajuwon@gmail.com>
2023-11-21 21:37:30 +0100

    initial

---
+++ tests/transaction.controller.test.js
@@ -0,0 +1,46 @@
+import { InsufficientBalance } from '../src/exceptions/transaction';
+import * as TransferRepository from '../src/models/transferModel';
+import * as UserModel from '../src/models/userModels';
+import Transaction from '../src/controller/transactionController';
+import { fetchUser } from '../src/middlewares/fetchUser';
+const creditTransaction = {
+	id: 1,
+	user_id: 1,
+	amount: 1000,
+	type: 'credit',
+	source: 'paymentProcessor',
+};
+
+const debitTransaction = {
+	id: 1,
+	user_id: 1,
+	amount: 1000,
+	type: 'debit',
+	source: 'paymentProcessor',
+};
+
+const jwtInfo = {
+	id: 1,
+	iat: 1660211932,
+	exp: 1660311932,
+};
+
+const user = {
+	id: 1,
+	email: 'mail@gmail.com',
+	account_balance: 100,
+};
+test('user get an error if they do not have enough balance', async () => {
+	const req = {
+		headers: { authorization: `Bearer ${jwtInfo}` },
+		body: { amount: 3000 },
+	};
+	jest
+		.spyOn(req.headers.authorization)
+		.mockImplementationOnce((token) => jwtInfo);
+	jest
+		.spyOn(UserModel, 'findUserById')
+		.mockImplementationOnce(() => new Promise((resolve) => resolve(user)));
+	const response = await Transaction.transfer(req, res);
+	expect(response.body.message).toBe(new InsufficientBalance().message);
+});
